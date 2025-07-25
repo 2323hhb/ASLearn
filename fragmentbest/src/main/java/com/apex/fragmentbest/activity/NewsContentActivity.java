62d82616eba28a9bdb91c061a26567f3fbc01c1d
2323hhb <1203451585@qq.com>
2025-07-25 14:18:05 +0800

    feat [fragmentbest] "更新碎片布局"

--- fragmentbest/src/main/java/com/apex/fragmentbest/activity/NewsContentActivity.java
+++ fragmentbest/src/main/java/com/apex/fragmentbest/activity/NewsContentActivity.java
@@ -4,11 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.apex.fragmentbest.R;
 import com.apex.fragmentbest.fragment.NewsContentFragment;
@@ -18,7 +14,7 @@ public class NewsContentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news_content);
+        setContentView(R.layout.news_content);
         String newsTitle=getIntent().getStringExtra("news_title");//获取传入的新闻标题
         String newsContent=getIntent().getStringExtra("news_content");//获取传入的新闻内容
         //在活动中调用碎片的方法，能够在活动中得到相应碎片的实例，然后就可以调用碎片里面的方法
